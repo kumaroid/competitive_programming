fe5c4c487d2ac0eab9b944f9218a47d168ac8f7c
kumaroid <113765449+kumaroid@users.noreply.github.com>
2024-02-08 01:52:40 +0300

    Create 896. Monotonic Array.py

---
+++ leetcode/896. Monotonic Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if nums == sorted(nums):
+            return True
+        elif nums == sorted(nums, reverse = True):
+            return True
+        return False
