5b01c3f2991f067d6612b920884b59d122469e5b
kumaroid <113765449+kumaroid@users.noreply.github.com>
2024-02-08 01:09:56 +0300

    Create 137. Single Number II.py

---
+++ leetcode/137. Single Number II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums):
+        count = defaultdict(int)
+        
+        for x in nums:
+            count[x] += 1
+
+        for x, freq in count.items():
+            if freq == 1:
+                return x
