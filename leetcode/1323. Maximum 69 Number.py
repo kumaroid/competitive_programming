05d521edd113466b2abb5079751ab36cc0acce58
kumaroid <113765449+kumaroid@users.noreply.github.com>
2024-02-08 01:41:51 +0300

    Create 1323. Maximum 69 Number.py

---
+++ leetcode/1323. Maximum 69 Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        temp = num
+        s = str(num)
+        for i in range(len(s)):
+            if s[i]=="6":
+                val = int(s[:i]+"9"+s[i+1:])
+            else:
+                val = int(s[:i]+"6"+s[i+1:]) 
+            temp = max(temp,val)
+        return temp
